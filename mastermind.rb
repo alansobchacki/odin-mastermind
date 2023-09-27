15e762f60de6fa35eec1a381242a8d6c810170c3
Alan Sobchacki <a.sobchack@gmail.com>
2023-09-27 14:18:55 -0300

    Will add the basic setup to allow the player to play as the codemaster.

--- mastermind.rb
+++ mastermind.rb
@@ -2,8 +2,8 @@
 
 require 'colorize'
 
-# This section is used to handle most of our messages, and the starting conditions of our game
-class GameStart
+# This class section is used to handle most of our messages, and the settings of our game
+class GameSettings
   attr_reader :choice
 
   def greetings
@@ -17,7 +17,7 @@ class GameStart
   def game_choice
     @choice = gets.chomp
     case
-    when @choice == '1' then puts "  You're the CODEMAKER! Pick your code and good luck!"
+    when @choice == '1' then puts "  You're the CODEMAKER! Build a four digit code with values ranging from 1 to 6!"
     when @choice == '2' then puts "  You're the CODEBREAKER! Good luck! You have 10 tries left."
     when @choice == '3' then tutorial
     else puts '  Please pick a valid option.' && game_choice
@@ -43,30 +43,66 @@ class GameStart
 
     game_choice
   end
+
+  def computer_is_codemaster
+    codemaster = Computer.new('')
+    codemaster.build_mastermind_code
+    codebreaker = Player.new(codemaster.computer_mastermind_code)
+    codebreaker.guessing
+    codebreaker.code_broken?
+  end
+
+  def player_is_codemaster
+    codemaster = Player.new('')
+    codemaster.build_mastermind_code
+    codebreaker = Computer.new(codemaster.mastermind_code)
+    codebreaker.guessing
+    codebreaker.code_broken?
+  end
 end
 
-# This section handles all moves made by the computer
+# This class section handles all moves made by the computer
 class Computer
-  attr_reader :mastermind_code
+  attr_reader :computer_mastermind_code
+
+  def initialize(mastermind_code)
+    @player_mastermind_code = mastermind_code
+  end
 
   def build_mastermind_code
-    @mastermind_code = []
-    @mastermind_code.push(rand(1..6).to_s) until @mastermind_code.length == 4
+    @computer_mastermind_code = []
+    @computer_mastermind_code.push(rand(1..6).to_s) until @computer_mastermind_code.length == 4
 
     puts '  *evil computer noises*'
     puts ''
   end
 end
 
-# This section handles all moves made by the player
+# This class section handles all moves made by the player
 class Player
+  attr_reader :player_mastermind_code
+
   def initialize(mastermind_code)
-    @mastermind_code = mastermind_code
+    @computer_mastermind_code = mastermind_code
+  end
+
+  def build_mastermind_code
+    @player_mastermind_code = gets.chomp.to_s.split('')
+    valid_digits = %w[1 2 3 4 5 6]
+
+    @player_mastermind_code.each do |element|
+      next unless valid_digits.include?(element) == false || @player_mastermind_code.length != 4
+
+      puts '  Invalid code. Please enter a four digit code with values ranging from 1 to 6.'
+      build_mastermind_code
+      break
+    end
   end
 
   def guessing
     @guess = gets.chomp.to_s.split('')
     valid_digits = %w[1 2 3 4 5 6]
+
     @guess.each do |element|
       next unless valid_digits.include?(element) == false || @guess.length != 4
 
@@ -78,17 +114,17 @@ class Player
 
   def code_broken?
     (1..11).each do |i|
-      if @guess == @mastermind_code
+      if @guess == @computer_mastermind_code
         puts '  You cracked the code! Great job!'
         break
-      elsif i < 10 && @guess != @mastermind_code
+      elsif i < 10 && @guess != @computer_mastermind_code
         puts "  Not quite. Try again! You have #{10 - i} tries left!"
-        getting_tips(@guess, @mastermind_code)
-      elsif i == 10 && @guess != @mastermind_code
+        getting_tips(@guess, @computer_mastermind_code)
+      elsif i == 10 && @guess != @computer_mastermind_code
         puts '  Not quite. Try again! This is your last chance!'
-        getting_tips(@guess, @mastermind_code)
-      elsif i > 10 && @guess != @mastermind_code
-        puts "  You didn't manage to crack the code. It was #{@mastermind_code}. Good luck next time!"
+        getting_tips(@guess, @computer_mastermind_code)
+      elsif i > 10 && @guess != @computer_mastermind_code
+        puts "  You didn't manage to crack the code. It was #{@computer_mastermind_code}. Good luck next time!"
       end
     end
   end
@@ -116,16 +152,14 @@ class Player
   end
 end
 
-newgame = GameStart.new
+newgame = GameSettings.new
 newgame.greetings
 newgame.game_choice
 
-if newgame.choice == '2'
-  codemaster = Computer.new
-  codemaster.build_mastermind_code
-  codebreaker = Player.new(codemaster.mastermind_code)
-  codebreaker.guessing
-  codebreaker.code_broken?
+if newgame.choice == '1'
+  newgame.player_is_codemaster
+elsif newgame.choice == '2'
+  newgame.computer_is_codemaster
 end
 
 ### things to do ###
